35c9ec18bbd2cbb3fc98daa2fc7c22fa812a279a
Jesus Carmona <ryzen3000@outlook.es>
2025-05-04 18:53:48 -0600

    My first commit, config in my project, I add paraglide and tailwind CSS

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+import {setLocale} from "$lib/paraglide/runtime.js";
+import {m} from '$lib/paraglide/messages.js'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>{m.hello_world({name: "JESUS"})}</p>
+<button onclick={() => setLocale('en')}> English</button>
+<button onclick={() => setLocale('es')}>Español</button>
