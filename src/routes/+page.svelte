ecab9ca51b7677e2de11784421d61427e4ed8f4e
Jesus Carmona <ryzen3000@outlook.es>
2025-05-06 23:58:26 -0600

    Ajuste del tema, footer y navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <script>
-import {setLocale} from "$lib/paraglide/runtime.js";
-import {m} from '$lib/paraglide/messages.js'
+    import Navbar from '$lib/components/navbar.svelte'
+    import Footer from '$lib/components/footer.svelte'
+    import {setLocale} from "$lib/paraglide/runtime.js";
+    import {m} from '$lib/paraglide/messages.js'
 </script>
+<Navbar></Navbar>
+<div class="max-w-screen-xl items-center mx-auto">
+    <h1>HOLA</h1>
+    <h2>HOLA</h2>
+    <h3>HOLA</h3>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>{m.hello_world({name: "JESUS"})}</p>
-<button onclick={() => setLocale('en')}> English</button>
-<button onclick={() => setLocale('es')}>Español</button>
+    <p>{m.intento()}</p>
+    <button onclick={() => setLocale('en')}> English</button>
+    <button onclick={() => setLocale('es')}>Español</button>
+</div>
+<Footer></Footer>
